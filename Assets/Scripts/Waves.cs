42650354d1a0d756812bd1ffb2a9bd39ce1664ef
Tonje Markseth <tonje.markseth@gmail.com>
2020-01-21 10:28:53 +0100

    Commit #1 - Testing unity in github

---
+++ Assets/Scripts/Waves.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+
+    public int Dimensions = 10;
+
+    protected MeshFilter MeshFilter;
+    protected Mesh Mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Mesh Setup
+        Mesh = new Mesh();
+        Mesh.name = gameObject.name;
+
+        Mesh.vertices = GenerateVerts();
+        Mesh.triangles = GenerateTries();
+        Mesh.RecalculateBound();
+
+        MeshFilter = gameObject.AddComponent<MeshFilter>();
+        MeshFilter.mesh = Mesh; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
