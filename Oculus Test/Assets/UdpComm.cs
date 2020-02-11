bdff2d2efce0efd1fe269f9f891185459cab5cb6
Tordal Sondre <Sondre.Tordal@macgregor.com>
2020-02-11 22:05:09 +0100

    Added UdpServer and tested with Oculus Quest

---
+++ Oculus Test/Assets/UdpComm.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net.Sockets;
+
+
+public class UdpComm : MonoBehaviour
+{
+
+    public byte[] txBuffer;
+    public int nSend;
+
+    private UdpClient udp;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        udp = new UdpClient(5678);
+        
+        
+
+        txBuffer = new byte[4];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        udp.Send(txBuffer, txBuffer.Length);
+
+        nSend = nSend + 1;
+
+    }
+}
---
+++ Oculus Test/Assets/UdpServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UdpServer : MonoBehaviour {
+
+
+    public int nRecv = 0;
+    public int nSend = 0;
+
+    public int rxSize = 0;
+    public int txSize = 0;
+    public byte[] rxBuffer;
+    public byte[] txBuffer;
+
+    private IPEndPoint remoteEP;
+
+    // UDP socket
+    UdpClient socket;
+
+
+    // Use this for initialization
+    void Start () {
+        // Start async UDP connection
+
+        socket = new UdpClient(5500);
+        socket.BeginReceive(new AsyncCallback(OnUdpData), socket);
+
+    }
+
+
+    void Update()
+    {
+        
+
+
+    }
+
+    void OnUdpData(IAsyncResult result)
+    {
+        // this is what had been passed into BeginReceive as the second parameter:
+        UdpClient socket = result.AsyncState as UdpClient;
+
+        // Recieve data from remote source
+        rxBuffer = socket.EndReceive(result, ref remoteEP);
+
+
+        // Echo data back to remote
+        socket.Send(rxBuffer, rxBuffer.Length, remoteEP);
+
+        nSend = nSend + 1;
+
+        // Schedule the next receive operation once reading is done:
+        socket.BeginReceive(new AsyncCallback(OnUdpData), socket);
+
+        // Increment counter
+        nRecv = nRecv + 1;
+    }
+
+    void OnApplicationQuit()
+    {
+        // Close UDP connection
+        socket.Close();
+    }
+}
+
