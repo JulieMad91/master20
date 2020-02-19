c074f6b0a2f1943a673f6bcbd8e160a10e5af773
Tonje Markseth <tonje.markseth@gmail.com>
2020-02-19 11:36:01 +0100

    testing terrain

---
+++ Oculus Test/Assets/playerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float runSpeed = 5f;
+    public float speed;
+    private float horizontal;
+    private float vertical;
+    private Rigidbody rb;
+    private playerController playerControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerControl = GetComponent<playerController>();
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        speed = rb.velocity.magnitude;
+
+        rb.velocity = new Vector3(horizontal * runSpeed, rb.velocity.y, vertical * runSpeed);
+    }
+}
