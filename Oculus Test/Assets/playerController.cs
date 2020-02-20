3b19f7a24afa21b11c7259853070c14408174fd2
Tonje Markseth <tonje.markseth@gmail.com>
2020-02-20 14:18:19 +0100

    Adding groundCheck script, removing rigidbody and adding character controller

--- Oculus Test/Assets/playerController.cs
+++ Oculus Test/Assets/playerController.cs
@@ -7,26 +7,40 @@ public class playerController : MonoBehaviour
 
     public float runSpeed = 5f;
     public float speed;
-    private float horizontal;
-    private float vertical;
-    private Rigidbody rb;
-    private playerController playerControl;
+    //private float horizontal;
+    //private float vertical;
+    public float gravity = 20.0f;
+    private CharacterController controller;
+
+    private Vector3 moveDirection = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        playerControl = GetComponent<playerController>();
+        //rb = GetComponent<Rigidbody>();
+       controller = GetComponent<CharacterController>();
         
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection *= runSpeed;
+        }
 
-        speed = rb.velocity.magnitude;
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
 
-        rb.velocity = new Vector3(horizontal * runSpeed, rb.velocity.y, vertical * runSpeed);
+
+        //horizontal = Input.GetAxis("Horizontal");
+        //vertical = Input.GetAxis("Vertical");
+
+        //speed = rb.velocity.magnitude;
+
+        //rb.velocity = new Vector3(horizontal * runSpeed, rb.velocity.y, vertical * runSpeed);
+
     }
 }
