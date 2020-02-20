5e8c52fafdeb40a7dcec2eab49424f21298b546d
JulieMad91 <31928463+JulieMad91@users.noreply.github.com>
2020-02-20 14:17:52 +0100

    Trying to figure out how to measure angles on terrain

--- Oculus Test/Assets/playerController.cs
+++ Oculus Test/Assets/playerController.cs
@@ -9,6 +9,14 @@ public class playerController : MonoBehaviour
     public float speed;
     private float horizontal;
     private float vertical;
+
+    public float h;
+    public Vector3 vec;
+
+    public Vector3 terrainSize;
+
+    private Terrain terrain;
+
     private Rigidbody rb;
     private playerController playerControl;
     // Start is called before the first frame update
@@ -24,9 +32,24 @@ public class playerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        h = rb.transform.position.y;
+        vec = transform.up;
 
-        speed = rb.velocity.magnitude;
+        // Read parent terrain
+        terrain = transform.GetComponentInParent<Terrain>();
 
-        rb.velocity = new Vector3(horizontal * runSpeed, rb.velocity.y, vertical * runSpeed);
+        terrainSize = terrain.terrainData.size;
+
+        // Normalized capsule location relative to terrain local coordinate
+        float rx = transform.position.x / terrainSize.x;
+        float rz = transform.position.z / terrainSize.z;
+
+        // Normal vector from terain directly under capsule
+        vec = terrain.terrainData.GetInterpolatedNormal(rx, rz);
+
+
+        //rb.velocity = new Vector3(horizontal * runSpeed * Time.deltaTime, rb.velocity.y, vertical * runSpeed * Time.deltaTime);
+        transform.position = transform.position + new Vector3(horizontal * runSpeed * Time.deltaTime, 0, vertical * runSpeed * Time.deltaTime);
+        //speed = rb.velocity.magnitude;
     }
 }
