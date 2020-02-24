66d6dbfd9b335a005bb119bc6057ba1d079762d9
JulieMad91 <31928463+JulieMad91@users.noreply.github.com>
2020-02-24 12:05:48 +0100

    Built environment

--- Oculus Test/Assets/playerController.cs
+++ Oculus Test/Assets/playerController.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
 
-    public float runSpeed = 5f;
-    public float speed;
+    public float runSpeed = 10f;
+    public float overallSpeed;
     //private float horizontal;
     //private float vertical;
-    public float gravity = 20.0f;
+    public float gravity = 0f;
     private CharacterController controller;
 
     private Vector3 moveDirection = Vector3.zero;
@@ -33,6 +33,7 @@ public class playerController : MonoBehaviour
 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+        overallSpeed = controller.velocity.magnitude;
 
 
         //horizontal = Input.GetAxis("Horizontal");
